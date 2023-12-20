fcb9fff1d0b7741fad6c281bf6b0e76dc77fc1e3
Elocodes <elochukwuodoeze@gmail.com>
2023-12-20 22:26:53 +0000

    added redis data retrieve funcs to class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ write to a redis server
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -23,3 +23,23 @@ class Cache():
         randkey = str(uuid.uuid4())
         self._redis. set(randkey, data)
         return randkey
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        retrieve data from Redis using the key
+        if conversion function fn is provided, convert data to utf-8 string, return
+        if key does not exist, return none
+        """
+        data = self._redis.get(key)
+        if data is not None:
+            return fn(data) if fn is not None else data
+        else:
+            return None
+
+    def get_str(self, key: str) -> str:
+        """shortcut func for callaing 'get' with str conversion"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """shortcut func for calling 'get' with int conversion"""
+        return self.get(key, fn=int)
