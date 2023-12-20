f4160fc3c5b4703b4753e530ec958707b1d9f7b9
Elocodes <elochukwuodoeze@gmail.com>
2023-12-20 21:40:50 +0000

    type-annotated data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,18 +5,20 @@ write to a redis server
 """
 import redis
 import uuid
+from typing import Union
 
 
 class Cache():
     """
     connect to redis server and store key-value pairs
+    type-annotate store method to reflect that data can be of diff types
     """
     def __init__(self):
         """connect to existing redis server, clear all existing keys"""
         self._redis = redis.Redis(host='localhost', port=6379)
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate random key using uuid, store data as key's value"""
         randkey = str(uuid.uuid4())
         self._redis. set(randkey, data)
